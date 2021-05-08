fd6420e0c194c5670867fafd48a51a838e5dd943
MalcolmOS <44449984+MalcolmOS@users.noreply.github.com>
2021-05-07 21:42:34 -0700

    Add files via upload
    Added error logging
    Added excel sheet logging
    Fixed variance condition and raised variance threshold to $10

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
+import sys
 import shutil
 from PyPDF2 import PdfFileMerger, PdfFileReader
+import openpyxl
 import datetime as dt
-import time
+import traceback
 
-INVOICES_PATH = ''
-REGISTERS_PATH = ''
-MATCHED_PATH = ''
+INVOICES_PATH = f"C:{os.path.sep}Users{os.path.sep}Malcolm{os.path.sep}Desktop{os.path.sep}Working Folder{os.path.sep}Buyers Invoices"
+REGISTERS_PATH = f"C:{os.path.sep}Users{os.path.sep}Malcolm{os.path.sep}Desktop{os.path.sep}Working Folder{os.path.sep}Buyers RR"
+MATCHED_PATH = f"C:{os.path.sep}Users{os.path.sep}Malcolm{os.path.sep}Desktop{os.path.sep}Working Folder{os.path.sep}Matched"
 
 
 class Matching:
@@ -21,6 +23,10 @@ class Matching:
         for invoice in self.invoices:
             # For each invoice, we search the registers for a match
             self.search_registers_for_match(invoice=invoice)
+        for invoice in self.invoices:
+            # For each invoice, we search the registers for a variance
+            self.search_registers_for_variance(invoice=invoice)
+
         self.report.send_report()
 
     def search_registers_for_match(self, invoice):
@@ -29,33 +35,41 @@ class Matching:
             if self.is_match(inv=invoice, register=rr):
                 # Ensures this is the only match, if it is we will merge, if not we will add to the report for A/P to look into manually.
                 if self.only_match(po=invoice['po'], amount=invoice['subtotal']):
-                    self.merge_files(invoice=invoice['file_name'], register=rr['file_name'])
+                    self.merge_files(invoice=invoice, register=rr)
                 else:
-                    self.report.append(f"More than one match for PO: {invoice['po']} Invoice: {invoice['number']} Amount: {invoice['subtotal']}")
-            elif self.has_variance(inv=invoice, register=rr):
-                # Notifies A/P of variances to ensure it's a match with a variance or not a match at all.
-                self.report.append(f"Found variance - PO: {invoice['po']} Invoice: {invoice['number']} Amount: {invoice['subtotal']}")
+                    self.report.append_multi_match(invoice=invoice)
+                self.invoices.remove(invoice)
+                self.registers.remove(rr)
+
+    def search_registers_for_variance(self, invoice):
+        for rr in self.registers:
+            # Notifies A/P of variances to ensure it's a match with a variance or not a match at all.
+            if self.has_variance(inv=invoice, register=rr):
+                self.report.append_variance(invoice=invoice, register=rr)
 
     def only_match(self, po, amount):
-        return self.only_invoice_match(po=po, amount=amount) and self.only_register_match(po=po, amount=amount)
+        return self.only_match_in_list(data=self.invoices, po=po, amount=amount) and self.only_match_in_list(data=self.registers, po=po, amount=amount)
 
-    # Counts how many invoices have this PO and subtotal
-    def only_invoice_match(self, po, amount):
+    def merge_files(self, invoice, register):
+        # Moves the invoice and register match to the matching folder and creates a third file for the merged PDF and saves the two separate documents as backup.
+        shutil.move(f"{INVOICES_PATH}{os.path.sep}{invoice['file_name']}", f"{MATCHED_PATH}{os.path.sep}{invoice['file_name']}")
+        shutil.move(f"{REGISTERS_PATH}{os.path.sep}{register['file_name']}", f"{MATCHED_PATH}{os.path.sep}{register['file_name']}")
+
+        merged_file = PdfFileMerger()
+        merged_file.append(PdfFileReader(f"{MATCHED_PATH}{os.path.sep}{invoice['file_name']}"))
+        merged_file.append(PdfFileReader(f"{MATCHED_PATH}{os.path.sep}{register['file_name']}"))
+        merged_file.write(f"{MATCHED_PATH}{os.path.sep}MATCHED-{invoice['number']}.pdf")
+
+        self.report.append_match(invoice=invoice, register=register)
+
+    @staticmethod
+    def only_match_in_list(data, po, amount):
+        # Searches the list and ensures only one match
         count = 0
-        for invoice in self.invoices:
+        for index in data:
             if count >= 2:
                 break
-            elif invoice['po'] == po and invoice['subtotal'] == amount:
-                count += 1
-        return count < 2
-
-    # Counts how many registers have this PO and subtotal
-    def only_register_match(self, po, amount):
-        count = 0
-        for rr in self.registers:
-            if count >= 2:
-                break
-            elif rr['po'] == po and rr['subtotal'] == amount:
+            elif index['po'] == po and index['subtotal'] == amount:
                 count += 1
         return count < 2
 
@@ -67,7 +81,7 @@ class Matching:
     @staticmethod
     def has_variance(inv, register):
         # compares the PO numbers and the subtotals and determines if there is a small variance
-        return register['po'] == inv['po'] and abs(float(inv['subtotal']) - float(register['subtotal'])) < 5
+        return register['po'] == inv['po'] and register['subtotal'] != inv['subtotal'] and abs(float(inv['subtotal']) - float(register['subtotal'])) <= 10
 
     @staticmethod
     def get_files(path):
@@ -80,45 +94,64 @@ class Matching:
                 f.append({'file_name': file, 'po': splits[0], "number": splits[1], 'subtotal': splits[2].replace(".pdf", "")})
         return f
 
-    @staticmethod
-    def merge_files(invoice, register):
-        # Moves the invoice and register match to the matching folder and creates a third file for the merged PDF and saves the two separate documents as backup.
-        print(f"Found match - Invoice: {invoice} - RR: {register}")
-        shutil.move(f"{INVOICES_PATH}{os.path.sep}{invoice}", f"{MATCHED_PATH}{os.path.sep}{invoice}")
-        shutil.move(f"{REGISTERS_PATH}{os.path.sep}{register}", f"{MATCHED_PATH}{os.path.sep}{register}")
-
-        merged_file = PdfFileMerger()
-        merged_file.append(PdfFileReader(f"{MATCHED_PATH}{os.path.sep}{invoice}"))
-        merged_file.append(PdfFileReader(f"{MATCHED_PATH}{os.path.sep}{register}"))
-        merged_file.write(f"{MATCHED_PATH}{os.path.sep}MATCHED-{invoice}")
-
 
 class MatchingReporting:
     def __init__(self):
         print("Loading the notification handler...")
-        self.output = ''
+        self.matches = []
+        self.variances = []
+        self.multiple_matches = []
+        self.wb = None
 
     def send_report(self):
-        print("Sending report")
-        print(self.output)
-        self.output = ''
+        self.wb = openpyxl.load_workbook(f'{MATCHED_PATH}{os.path.sep}AutoMatchReport.xlsx')
+        self.save_matches()
+        self.save_variances()
+        self.save_multiple_matches()
+        self.wb.save(f'{MATCHED_PATH}{os.path.sep}AutoMatchReport.xlsx')
+        self.wb.close()
 
-    def append(self, content):
-        self.output += f'{content}\n'
+    def save_matches(self):
+        self.wb.active = self.wb['Matches']
+        for match in self.matches:
+            self.wb.active.append(match)
+
+    def save_variances(self):
+        self.wb.active = self.wb['Variances']
+        for variance in self.variances:
+            self.wb.active.append(variance)
+
+    def save_multiple_matches(self):
+        self.wb.active = self.wb['MultipleMatches']
+        for match in self.multiple_matches:
+            self.wb.active.append(match)
+
+    def append_match(self, invoice, register):
+        print(f"Found match - Invoice: {invoice['file_name']} - RR: {register['file_name']}")
+        self.matches.append((dt.datetime.today().date(), invoice['number'], register['number']))
+
+    def append_variance(self, invoice, register):
+        print(f"Found variance - PO: {invoice['po']} Invoice: {invoice['number']} Amount: {invoice['subtotal']}")
+        self.variances.append((dt.datetime.today().date(), invoice['number'], register['number']))
+
+    def append_multi_match(self, invoice):
+        print(f"More than one match for PO: {invoice['po']} Invoice: {invoice['number']} Amount: {invoice['subtotal']}")
+        self.multiple_matches.append((dt.datetime.today().date(), invoice['po'], invoice['number']))
+
+
+def print_stack():
+    print(traceback.format_exc())
+    exception_type, exception_object, exception_traceback = sys.exc_info()
+    filename = exception_traceback.tb_frame.f_code.co_filename
+    line_number = exception_traceback.tb_lineno
+    exception_message = f'Exception Type: {exception_type} - File Name: {filename} - Line Number: {line_number}'
+    f = open(f'{MATCHED_PATH}{os.path.sep}ErrorLog.txt', "a")
+    f.write(exception_message)
+    f.close()
 
 
 if __name__ == '__main__':
-
-    print("Starting...")
-    while True:
-        try:
-            # Best to run as a scheduled runtime and not leave a background process but if we ran it as a background process we can do this.
-            # If the hour of the day is midnight, we will run this matching code.
-            if dt.datetime.today().hour == 0:
-                Matching().run()
-        except Exception as e:
-            # Prints out any errors should they happen
-            print(e)
-        finally:
-            # sleeps for one hour until re-looping and starting over
-            time.sleep(3600)
+    try:
+        Matching().run()
+    except Exception as e:
+        print_stack()
